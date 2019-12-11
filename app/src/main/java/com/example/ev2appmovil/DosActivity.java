7f4de5a7f7fe24430c0e25481b42728755ff792c
Moixe <eosoriom@gmail.com>
2019-12-11 01:45:54 -0300

    prueba 2

--- app/src/main/java/com/example/ev2appmovil/DosActivity.java
+++ app/src/main/java/com/example/ev2appmovil/DosActivity.java
@@ -2,13 +2,109 @@ package com.example.ev2appmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class DosActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> adaptador;
+    private ArrayList<String> lasTareas;
+    private ListView lstTareas;
+    private Button btnFiltrar;
+    private TextView tilBusqueda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dos);
+        referencias();
+        eventos();
+        leer();
+        adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lasTareas);
+        lstTareas.setAdapter(adaptador);
+
+    }
+
+    public void referencias() {
+        lstTareas = findViewById(R.id.lvItems);
+        lasTareas = new ArrayList();
+        tilBusqueda = findViewById(R.id.tietBuscar);
+        btnFiltrar = findViewById(R.id.btnFiltrar);
+    }
+
+    public void eventos() {
+
+        btnFiltrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lasTareas.clear();
+                SharedPreferences p = getSharedPreferences("Tareas", Context.MODE_PRIVATE);
+                Map<String, ?> claves = p.getAll();
+
+                if (tilBusqueda.getText().toString().isEmpty()) {
+                    leer();
+                } else {
+                    int val = 0;
+                    for (Map.Entry<String, ?> entrada : claves.entrySet()) {
+                        if (entrada.getKey().contains(tilBusqueda.getText().toString()) || entrada.getValue().toString().contains(tilBusqueda.getText().toString())) {
+                            lasTareas.add(entrada.getKey() + " : " + entrada.getValue().toString());
+                            val = 1;
+                        }
+                    }
+                    if (val == 0) {
+                        lasTareas.add("no hay registro");
+                    }
+                }
+                lstTareas.setAdapter(adaptador);
+            }
+        });
+
+        lstTareas.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view,int i, long l) {
+
+                SharedPreferences p = getSharedPreferences("Tareas", Context.MODE_PRIVATE);
+
+
+                Snackbar sb = Snackbar.make(view, "este es un mensaje" + i + l , Snackbar.LENGTH_LONG);
+                sb.setAction("aceptar", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        });
+                sb.show();
+
+
+
+                lasTareas.remove(i);
+                lstTareas.setAdapter(adaptador);
+              //  Toast.makeText(DosActivity.this,"click" + lasTareas.remove(i), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+
+    public void leer() {
+        SharedPreferences p = getSharedPreferences("Tareas", Context.MODE_PRIVATE);
+
+        Map<String, ?> claves = p.getAll();
+        for (Map.Entry<String, ?> entrada : claves.entrySet()) {
+            lasTareas.add(entrada.getKey() + " : " + entrada.getValue().toString());
+        }
     }
 }
--- app/src/main/java/com/example/ev2appmovil/MainActivity.java
+++ app/src/main/java/com/example/ev2appmovil/MainActivity.java
@@ -2,13 +2,104 @@ package com.example.ev2appmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnBuscar, btnIngresar, btnModificar;
+    private TextView tilTitulo, tilDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        referencias();
+        eventos();
     }
+
+    private void eventos() {
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, DosActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validacion()) {
+                    SharedPreferences p = getSharedPreferences("Tareas", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editarArchivo = p.edit();
+                    editarArchivo.putString(tilTitulo.getText().toString(), tilDesc.getText().toString());
+                    editarArchivo.commit();
+                    tilTitulo.setText("");
+                    tilDesc.setText("");
+                    Toast.makeText(getApplication(), "registro exitoso", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validacion()) {
+                    SharedPreferences p = getSharedPreferences("Tareas", Context.MODE_PRIVATE);
+                    Map<String, ?> claves = p.getAll();
+                    int toast = 0;
+                    for (Map.Entry<String, ?> entrada : claves.entrySet()) {
+                        if (entrada.getKey().contentEquals(tilTitulo.getText())) {
+                            SharedPreferences.Editor editarArchivo = p.edit();
+                            editarArchivo.putString(tilTitulo.getText().toString(), tilDesc.getText().toString());
+                            editarArchivo.apply();
+                            tilTitulo.setText("");
+                            tilDesc.setText("");
+                            toast = 1;
+                            break;
+                        }
+                    }
+                    if (1 != toast)
+                        Toast.makeText(getApplication(),R.string.error_NoExiste, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void referencias() {
+        btnBuscar = findViewById(R.id.btnSearch);
+        btnModificar = findViewById(R.id.btnModificar);
+        btnIngresar = findViewById(R.id.btnIngresar);
+        tilTitulo = findViewById(R.id.tietTitulo);
+        tilDesc = findViewById(R.id.tietDesc);
+    }
+
+    private boolean validacion() {
+        boolean validacion = true;
+
+        if (tilTitulo.getText().toString().trim().isEmpty()) {
+            tilTitulo.setError(MainActivity.this.getString(R.string.error_Titulo));
+            validacion = false;
+        }
+        if (tilDesc.getText().toString().trim().isEmpty()) {
+            tilDesc.setError(MainActivity.this.getString(R.string.error_Desc));
+            validacion = false;
+        }
+        if (validacion) {
+        } else {
+            Toast.makeText(getApplication(), R.string.error_Validacion, Toast.LENGTH_LONG).show();
+        }
+        return validacion;
+    }
+
 }
